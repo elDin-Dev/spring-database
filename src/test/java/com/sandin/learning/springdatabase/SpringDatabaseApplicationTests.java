4cb57ff7068d570cf24f0946325315a97691c3b7
sandina <alejandro.sandin@roche.com>
2022-01-12 15:38:41 +0100

    Initial commit

---
+++ src/test/java/com/sandin/learning/springdatabase/SpringDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandin.learning.springdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
